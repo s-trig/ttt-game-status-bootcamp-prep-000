bf5351c858005441bd350797707ce942341b4f8e
David Reep <davereep@gmail.com>
2017-10-26 20:49:07 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,3 +21,12 @@ def full?(board)
     i == "X" || i == "O"
   end
 end
+
+def draw(board) 
+  if !won?(board) && full?(board)
+    return true
+  elsif 
+    won?(board) || !full?(board)
+    return false
+  end
+end
