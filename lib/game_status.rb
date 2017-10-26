4c78ca90c1e64e605f59fa555dda8d9f025d4334
David Reep <davereep@gmail.com>
2017-10-26 20:43:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,10 @@ def won?(board)
 end
 
 def full?(board)
-  board.all? do |i|
-    i == "X" || i == "O"
+  board.select do |i|
+    if i == "X" || i == "O"
+      return true
+    end
+    return false
   end
 end
