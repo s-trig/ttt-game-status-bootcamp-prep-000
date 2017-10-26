974021aab9fc99153620a62ac8d5ebe05c5ced48
David Reep <davereep@gmail.com>
2017-10-26 20:10:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,7 +13,8 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.detect do |i|
   if board[i[0]] == board[i[1]] && board[i[1]] == board[i[2]] && position_taken?(board, win_index[0])
+  else 
+    return false
   end
 end
-return false
 end
